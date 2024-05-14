905592d514f6271cbe3a621445e59b1e885183a7
rezha4 <rezha.bahari@gmail.com>
2024-05-14 13:34:51 +0700

    adding clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  ClerkProvider,
+} from "@clerk/nextjs";
 
 export const rubik = Rubik({
   subsets: ["latin"],
@@ -19,15 +26,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          rubik.className
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <header>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            rubik.className
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
