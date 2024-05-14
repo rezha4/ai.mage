c09939befc11f1ab696e7727e7d2b657bb851966
rezha4 <rezha.bahari@gmail.com>
2024-05-14 18:03:35 +0700

    user schema

--- lib/database/models/user.models.ts
+++ lib/database/models/user.models.ts
@@ -1,5 +1,24 @@
-import { Schema } from "mongoose";
+import { Schema, model, models } from "mongoose";
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  clerkId: string;
+  planId: string;
+  photo: string;
+  creditBalance: number;
+}
 
 const UserSchema = new Schema({
-  
-})
+  firstName: { type: String },
+  lastName: { type: String },
+  clerkId: { type: String, required: true, unique: true },
+  planId: { type: Number, default: 1 },
+  photo: { type: String, required: true },
+  creditBalance: { type: Number, default: 10 },
+  email: { type: String, required: true, unique: true },
+});
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
