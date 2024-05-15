edc5f7b4b39999d1e04612cae1f571a0861a6eca
rezha4 <rezha.bahari@gmail.com>
2024-05-15 16:29:58 +0700

    sidebar for dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,59 @@
-import { Navbar } from "@/components/shared/navbar";
-import React, { ReactNode } from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React, { ReactNode, useState } from "react";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <>
-      <Navbar />
-      {children}
-    </>
+    <div className="flex">
+      {showSidebar ? (
+        <aside
+          className={`${
+            showSidebar ? "flex" : "hidden"
+          } 
+          transform flex-col gap-4 justify-between items-center p-4 shadow-xl h-screen w-1/4 bg-gradient-to-b from-slate-200 to-slate-400`}
+        >
+          <div className="space-y-8">
+            <h2 className="text-3xl font-serif flex items-center justify-around">
+              AI.mage
+            </h2>
+            <div className="flex flex-col space-y-4">
+              <Button variant="secondary">
+                <Link href={"#"}>Remove Background</Link>
+              </Button>
+              <Button variant="secondary">
+                <Link href={"#"}>Remove Background</Link>
+              </Button>
+              <Button variant="secondary">
+                <Link href={"#"}>Remove Background</Link>
+              </Button>
+              <Button variant="secondary">
+                <Link href={"#"}>Remove Background</Link>
+              </Button>
+              <Button variant="secondary">
+                <Link href={"#"}>Remove Background</Link>
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 items-center">
+            <Button>Buy more credits</Button>
+            <UserButton />
+          </div>
+        </aside>
+      ) : (
+        <div className="mt-4 ml-4">
+          <Button onClick={() => setShowSidebar((prev) => !prev)}>
+            <HamburgerMenuIcon />
+          </Button>
+        </div>
+      )}
+      <div onClick={() => setShowSidebar(false)} className="p-4 h-screen">{children}</div>
+    </div>
   );
 };
 
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { useEffect, useState } from "react";
+
 const Dashboard = () => {
+  // const [user, setUser] = useState<string>("");
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const fetchedUser = await currentUser();
+  //     setUser(fetchedUser);
+  //   }
+
+  //   fetchUser();
+  // }, [])
+
   return (
     <div>
-      dashboard
+      <h2 className="text-2xl">Welcome!</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
